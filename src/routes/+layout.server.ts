b28e17b0ea907b39ab81b51fc691a2b186663ab9
CactusPuppy <puppycactus@gmail.com>
2025-11-24 11:31:28 -0800

    feat: login/homepage redirects

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,8 @@
+import type { LayoutServerLoad } from "./$types";
+
+export const load: LayoutServerLoad = async ({ locals }) => {
+  const auth = await locals.auth();
+  return {
+    user: auth?.user || null
+  };
+};
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,6 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+export const load: PageServerLoad = async (event) => {
+  const auth = await event.locals.auth();
+  if (!auth?.user) redirect(303, "/login");
+};
